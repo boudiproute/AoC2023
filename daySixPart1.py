c0c441790b487d251a97b57b3f3b0fc1db50aa59
Adam Fortin <afortin@nitrof.com>
2023-12-06 15:39:26 -0500

    Needed to take out high school homework to solve

--- daySixPart1.py
+++ daySixPart1.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import math
 
 #file = open(sys.argv[1], 'r')
 file = open('DaySixInput.txt', 'r')
@@ -17,11 +18,15 @@ for line in file:
 
 for races in range(0,len(data[0])):
     recordBraking = 0 # How many recordbraking millisecond per race
-    for time in range(0,data[0][races]):
-        race[0].append(time)
-        race[1].append((data[0][races]-time)*time) #Total time of race - time pressed * speed (which = time pressed)
-    for distance in race[1]:
-        if distance > data[1][races]:
-            recordBraking += 1
-    recordWay = recordWay * recordBraking
+
+    a = -1
+    b = data[0][races]
+    c = -data[1][races]
+    quad = math.sqrt(b**2-4*a*c)
+
+    x1 = math.floor((b + quad) / (2 * a)) 
+    x2 = math.ceil((b - quad) / (2 * a)) - 1
+
+    recordBraking = abs(x2-x1)
+    recordWay *= recordBraking
 print(recordWay)
